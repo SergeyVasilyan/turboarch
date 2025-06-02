5a65da4f38cb23b7cff249ace9d383fd713ddbc1
Sergey Vasilyan <yegress110@gmail.com>
2025-06-02 08:13:45 +0400

    Description:
        - correct tab levels,
        - remove "neofetch" installation as it fails to be installed and
          crashes the whole process.
        - remove pearl comments.

--- stage2.sh
+++ stage2.sh
@@ -12,8 +12,8 @@ echo -e "\e[1m\e[100m\e[97mFOUND $total FILES\e[0m"
 echo -e "\e[1m\e[41m\e[97mDESTROYING HOST SYSTEM IN 5 SECONDS\e[0m"
 
 for i in 5 4 3 2 1; do
-  printf "%s..." "$i"
-  sleep 1
+    printf "%s..." "$i"
+    sleep 1
 done
 printf "\n"
 
@@ -21,7 +21,7 @@ echo -e "\e[1m\e[41m\e[97mDESTROYING HOST SYSTEM\e[0m"
 rm -rvf bin etc lib lib64 sbin srv usr var |& { I=0; while read -r; do printf "%s files removed\r" "$((++I))"; done; echo ""; }
 echo -e "\e[1m\e[41m\e[97mHOST SYSTEM DESTROYED\e[0m"
 
-cd / 
+cd /
 
 echo -e "\e[1m\e[46m\e[97mSETING UP PACMAN\e[0m"
 
@@ -36,20 +36,19 @@ pacman-key --populate
 
 
 if [ "$REFLECTOR" -eq 1  ]; then
-  # взрыв мозга блять
-  mkdir /thisroot
-  mount --bind / /thisroot
+    mkdir /thisroot
+    mount --bind / /thisroot
 
-  echo -e "\e[1m\e[46m\e[97mINSTALLING REFLECTOR\e[0m"
-  pacstrap /thisroot reflector
+    echo -e "\e[1m\e[46m\e[97mINSTALLING REFLECTOR\e[0m"
+    pacstrap /thisroot reflector
 
-  echo -e "\e[1m\e[46m\e[97mRUNNING REFLECTOR (it will take some time)\e[0m"
-  reflector --country Russia,France,Netherlands --latest 12 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+    echo -e "\e[1m\e[46m\e[97mRUNNING REFLECTOR (it will take some time)\e[0m"
+    reflector --country Russia,France,Netherlands --latest 12 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 fi
 
 
 echo -e "\e[1m\e[46m\e[97mINSTALLING BASE SYSTEM\e[0m"
-pacstrap -K /host-system base grub neofetch sudo vim efibootmgr xfsprogs btrfs-progs dhcpcd wpa_supplicant
+pacstrap -K /host-system base grub sudo vim efibootmgr xfsprogs btrfs-progs dhcpcd wpa_supplicant
 
 echo -e "\e[1m\e[46m\e[97mCOPYING FSTAB\e[0m"
 
@@ -59,13 +58,12 @@ cp /host-system/turboarch-config/crypttab /host-system/etc/crypttab
 chmod +x /host-system/turboarch-config/stage3.sh
 
 if [ -f /host-system/turboarch-config/passwd_delta ]; then
-  echo -e "\e[1m\e[46m\e[97mCONFIGURING USERS\e[0m"
+    echo -e "\e[1m\e[46m\e[97mCONFIGURING USERS\e[0m"
 
-  for word in passwd shadow group gshadow; do
-    # ПРИМИТЕ МЕНЯ РАБОТАТЬ В БАМЖАРО ХРЮНИКС!!!
-    jopa=$(tail -n+2 /host-system/etc/${word})
-    echo -e "$(grep '^root:' /host-system/turboarch-config/${word}_delta)\n$jopa\n$(grep -v '^root:' /host-system/turboarch-config/${word}_delta)\n" | grep "\S" > /host-system/etc/${word}
-  done
+    for word in passwd shadow group gshadow; do
+        jopa=$(tail -n+2 /host-system/etc/${word})
+        echo -e "$(grep '^root:' /host-system/turboarch-config/${word}_delta)\n$jopa\n$(grep -v '^root:' /host-system/turboarch-config/${word}_delta)\n" | grep "\S" > /host-system/etc/${word}
+    done
 fi
 
 echo -e "\e[1m\e[46m\e[97mEXECUTING CHROOT TO NEW SYSTEM\e[0m"
--- stage3.sh
+++ stage3.sh
@@ -3,22 +3,22 @@
 echo -e "\e[1m\e[46m\e[97mSTAGE 3 ACTIVATED\e[0m"
 
 if [ -f /turboarch-config/wheel_users ]; then
-  while IFS="" read -r p || [ -n "$p" ]
-  do
-    echo -e "\e[1m\e[46m\e[97mADD USER $p TO GROUP wheel\e[0m"
-    usermod -a -G wheel "$p"
-  done < /turboarch-config/wheel_users
+    while IFS="" read -r p || [ -n "$p" ]
+    do
+        echo -e "\e[1m\e[46m\e[97mADD USER $p TO GROUP wheel\e[0m"
+        usermod -a -G wheel "$p"
+    done < /turboarch-config/wheel_users
 
-  echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00_wheel
+    echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00_wheel
 fi
 
 if [ -f /turboarch-config/passwd_delta ]; then
-  while IFS="" read -r p || [ -n "$p" ]
-  do
-    IFS=':' read -r -a arr <<< "$p"
-    echo -e "\e[1m\e[46m\e[97mCHOWN HOME DIRECTORY ${arr[5]} FOR USER ${arr[0]}\e[0m"
-    chown -R "${arr[0]}:${arr[0]}" "${arr[5]}" 
-  done < /turboarch-config/passwd_delta
+    while IFS="" read -r p || [ -n "$p" ]
+    do
+        IFS=':' read -r -a arr <<< "$p"
+        echo -e "\e[1m\e[46m\e[97mCHOWN HOME DIRECTORY ${arr[5]} FOR USER ${arr[0]}\e[0m"
+        chown -R "${arr[0]}:${arr[0]}" "${arr[5]}"
+    done < /turboarch-config/passwd_delta
 fi
 
 source /turboarch-config/config
@@ -31,83 +31,79 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "$NEWHOSTNAME" > /etc/hostname
 
 if [ "$SET_SPACE_PASSWORD" -eq 1  ]; then
-     echo "root: " | chpasswd
+    echo "root: " | chpasswd
 fi
 
 rm -rf /boot/*
 
 
-# установить ЕБУЧИЙ СРАКУТ и хрюки пекмена для него
 if [ "$SRAKUT" -eq 1  ]; then
-  echo -e "\e[1m\e[40m\e[93mINSTALLING DRACUT AND LVM2\e[0m"
-  pacman --noconfirm -Sy lvm2 mdadm dracut
+    echo -e "\e[1m\e[40m\e[93mINSTALLING DRACUT AND LVM2\e[0m"
+    pacman --noconfirm -Sy lvm2 mdadm dracut
 
-  echo -e "\e[1m\e[40m\e[93mINSTALL DRACUT HOOKS\e[0m"
+    echo -e "\e[1m\e[40m\e[93mINSTALL DRACUT HOOKS\e[0m"
 
-  install -Dm644 /turboarch-config/90-dracut-install.hook /usr/share/libalpm/hooks/90-dracut-install.hook
-  install -Dm644 /turboarch-config/60-dracut-remove.hook /usr/share/libalpm/hooks/60-dracut-remove.hook
-  install -Dm755 /turboarch-config/dracut-install /usr/share/libalpm/scripts/dracut-install
-  install -Dm755 /turboarch-config/dracut-remove /usr/share/libalpm/scripts/dracut-remove
-  
-  echo -e "\e[1m\e[40m\e[93mINSTALL KERNEL AND RUN DRACUT HOOKS\e[0m"
-  pacman --noconfirm -Rns mkinitcpio
+    install -Dm644 /turboarch-config/90-dracut-install.hook /usr/share/libalpm/hooks/90-dracut-install.hook
+    install -Dm644 /turboarch-config/60-dracut-remove.hook /usr/share/libalpm/hooks/60-dracut-remove.hook
+    install -Dm755 /turboarch-config/dracut-install /usr/share/libalpm/scripts/dracut-install
+    install -Dm755 /turboarch-config/dracut-remove /usr/share/libalpm/scripts/dracut-remove
+
+    echo -e "\e[1m\e[40m\e[93mINSTALL KERNEL AND RUN DRACUT HOOKS\e[0m"
+    pacman --noconfirm -Rns mkinitcpio
 fi
 
 pacman --noconfirm -Sy linux linux-firmware
 
 
 if [ "$GNOME" -eq 1  ]; then
-  echo -e "\e[1m\e[46m\e[97mINSTALLING GNOME\e[0m"
-  pacman --noconfirm -Sy gnome gnome-tweaks
-  systemctl enable gdm
+    echo -e "\e[1m\e[46m\e[97mINSTALLING GNOME\e[0m"
+    pacman --noconfirm -Sy gnome gnome-tweaks
+    systemctl enable gdm
 
-  # удалить ебаные темы колхозанские блять и прочий мусор от старой системы
   for d in /home/*/ ; do
-    echo "$d"
-    rm -rf "$d/.config/dconf"
-    rm -rf "$d/.config/gtk-3.0"
-    rm -rf "$d/.config/gtk-4.0"
-    rm -rf "$d/.cache/*"
+      echo "$d"
+      rm -rf "$d/.config/dconf"
+      rm -rf "$d/.config/gtk-3.0"
+      rm -rf "$d/.config/gtk-4.0"
+      rm -rf "$d/.cache/*"
 
-    rm -rf "$d/.local/share/themes"
-    rm -rf "$d/.local/share/icons"
+      rm -rf "$d/.local/share/themes"
+      rm -rf "$d/.local/share/icons"
 
-    rm -rf "$d/.themes"
-    rm -rf "$d/.icons"
+      rm -rf "$d/.themes"
+      rm -rf "$d/.icons"
   done
 fi
 
 if [ "$NETWORKMANAGER" -eq 1  ]; then
-  echo -e "\e[1m\e[46m\e[97mINSTALLING NETWORKMANAGER\e[0m"
-  pacman --noconfirm -Sy networkmanager
-  systemctl enable NetworkManager
+    echo -e "\e[1m\e[46m\e[97mINSTALLING NETWORKMANAGER\e[0m"
+    pacman --noconfirm -Sy networkmanager
+    systemctl enable NetworkManager
 fi
 
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 
 if [ -d /sys/firmware/efi ]; then
-  echo -e "\e[1m\e[46m\e[97mINSTALLING GRUB (UEFI)\e[0m"
-  env -i grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCHGRUB || env -i grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCHGRUB
+    echo -e "\e[1m\e[46m\e[97mINSTALLING GRUB (UEFI)\e[0m"
+    env -i grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCHGRUB || env -i grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCHGRUB
 else
-  dev=$(findmnt -n -o SOURCE /boot | sed 's/ .*//;s/\/dev\///;s/\[.*//') 
-  if [ -z "$dev" ]; then
-    dev=$(findmnt -n -o SOURCE / | sed 's/ .*//;s/\/dev\///;s/\[.*//') 
-  fi
-  target=$(basename "$(readlink -f "/sys/class/block/$dev/..")")
+    dev=$(findmnt -n -o SOURCE /boot | sed 's/ .*//;s/\/dev\///;s/\[.*//')
+    if [ -z "$dev" ]; then
+        dev=$(findmnt -n -o SOURCE / | sed 's/ .*//;s/\/dev\///;s/\[.*//')
+    fi
+    target=$(basename "$(readlink -f "/sys/class/block/$dev/..")")
 
-  if [ -z "$target" ]; then
-    echo -e "\e[1m\e[40m\e[93mCANNOT FIND DEVICE ON / FOR SOME REASON\e[0m"
-    lsblk
-    read -p "Enter drive for GRUB installation (e.g. sda or nvme0n1): " -r target
+    if [ -z "$target" ]; then
+        echo -e "\e[1m\e[40m\e[93mCANNOT FIND DEVICE ON / FOR SOME REASON\e[0m"
+        lsblk
+        read -p "Enter drive for GRUB installation (e.g. sda or nvme0n1): " -r target
 
-  fi
-  #echo -e "\e[1m\e[46m\e[97mWIPING MBR FROM TARGET DRIVE: \e[0m" 
-  #dd if=/dev/zero of=/dev/sdx bs=446 count=1
+    fi
 
-  echo -e "\e[1m\e[46m\e[97mINSTALLING GRUB (BIOS) ON $target\e[0m" 
-  env -i grub-install --target=i386-pc "/dev/$target"
-  cp -r /usr/lib/grub/i386-pc /boot/grub
+    echo -e "\e[1m\e[46m\e[97mINSTALLING GRUB (BIOS) ON $target\e[0m"
+    env -i grub-install --target=i386-pc "/dev/$target"
+    cp -r /usr/lib/grub/i386-pc /boot/grub
 fi
 
 echo -e "\e[1m\e[46m\e[97mCREATING GRUB CONFIG\e[0m"
@@ -116,9 +112,9 @@ env -i grub-mkconfig -o /boot/grub/grub.cfg
 sync
 
 if [[ "$FORCE_REBOOT_AFTER_INSTALLATION" != "1" ]]; then
-  echo -e "\e[1m\e[46m\e[97mSTARTING BASH TO PERFORM MANUAL POST-INSTALL CONFIGURATION\e[0m"
-  echo -e "EXIT TO REBOOT"
-  bash
+    echo -e "\e[1m\e[46m\e[97mSTARTING BASH TO PERFORM MANUAL POST-INSTALL CONFIGURATION\e[0m"
+    echo -e "EXIT TO REBOOT"
+    bash
 fi
 
 echo -e "\e[1m\e[46m\e[97mREBOOTING SYSTEM NOW\e[0m"
